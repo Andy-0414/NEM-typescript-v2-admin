c73ee47279ee93598b00b026aa4c146cfafb0442
Andy0414 <pjh8667@naver.com>
2020-06-10 09:18:34 +0900

    Update main

--- src/views/Home/Main.vue
+++ src/views/Home/Main.vue
@@ -1,5 +1,7 @@
 <template>
-	<div class="main">Main</div>
+	<div class="main">
+		<div class="main__content">NEM TYPESCRIPT ADMIN CONSOLE</div>
+	</div>
 </template>
 
 <script lang="ts">
@@ -10,4 +12,14 @@ export default class Main extends Vue {}
 </script>
 
 <style lang="scss" scoped>
+.main {
+	padding: 20px;
+	.main__content {
+		padding: 30px;
+
+		border-radius: 10px;
+		background-color: $block-color;
+		box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.1);
+	}
+}
 </style>
